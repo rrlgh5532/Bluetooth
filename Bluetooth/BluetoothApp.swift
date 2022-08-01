5a488f007c83ef34da1cf592cd76fd71f07f78af
One27 <rrlgh5532@me.com>
2022-08-02 01:02:23 +0900

    Initial Commit

---
+++ Bluetooth/BluetoothApp.swift
@@ -0,0 +1,17 @@
+//
+//  BluetoothApp.swift
+//  Bluetooth
+//
+//  Created by 이기호 on 2022/08/02.
+//
+
+import SwiftUI
+
+@main
+struct BluetoothApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
